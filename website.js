5d965c6ae11161328e87e12e1d88df9dd92d8130
Richard Medina <Rmedina191@hotmail.com>
2022-02-12 00:03:57 -0500

    first commit

---
+++ website.js
@@ -0,0 +1,17 @@
+let add1 = document.querySelector('.add1');
+
+add1.addEventListener('click', function(){
+    alert(`Berserker Armor Guts has been added to the cart!`);
+});
+
+let add2 = document.querySelector('.add2');
+
+add2.addEventListener('click', function(){
+    alert(`Captain Tashigi has been added to the cart!`);
+});
+
+let add3 = document.querySelector('.add3');
+
+add3.addEventListener('click', function(){
+    alert(`Spike Spiegel has been added to the cart!`);
+});
